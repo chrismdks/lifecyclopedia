dd8d1736d37f4ce3a02095374078956745ccd82d
chrismdks <chris.mdks@gmail.com>
2023-04-10 08:53:47 +0300

    loaded Instructor data to initial state - componentDidMount() method

--- src/ClassPage.jsx
+++ src/ClassPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { getRandomUser } from "./Utility/api";
 
 class ClassPage extends React.Component{
     constructor(props){
@@ -11,7 +12,54 @@ class ClassPage extends React.Component{
         };
     }
 
+    // 3 critical lifecycle methods for class-based components:
+    
+    /** componentDidMount(): 
+     * the best place to load some data to my initial state is through this method (in a class-based component).
+     * It is invoked immediately after a component is mounted.
+    */
+    componentDidMount = async() => {
+        console.log("Component Did Mount");
+        const response = await getRandomUser(); //getRandomUser() is an async method so we have to await.
+        console.log(response);
+        this.setState(prevState => {
+            return {
+                instructor: {
+                    name: response.data.first_name + " " + response.data.last_name,
+                    email: response.data.email,
+                    phone: response.data.phone_number
+                }
+            }
+        });
+    }
+
+    componentDidUpdate(){
+        console.log("Component Did Update");
+    }
+
+    componentWillUnmount(){
+        console.log("Component Will Unmount");
+    }
+
     render(){
-        
+        console.log("Render Component");
+        return (
+            <div>
+                {this.state.instructor && (
+                    <div className="p-3">
+                        <span className="h4 text-success">Instructor</span>
+                        <i className="bi bi-toggle-off btn btn-success btn-sm mb-1"></i>
+                        <br/>
+                        Name: {this.state.instructor.name}
+                        <br/>
+                        Email: {this.state.instructor.email}
+                        <br/>
+                        Phone: {this.state.instructor.phone}
+                    </div>
+                )}
+            </div>
+        )
     }
 }
+
+export default ClassPage
